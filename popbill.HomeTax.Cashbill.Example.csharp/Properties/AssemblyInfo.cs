8309ef5d89b4ac4da5a84187511390c369c6d2ce
JohnJeong123 <frenchofkiss@gmail.com>
2016-07-06 14:00:42 +0900

    upper case package name

--- popbill.HomeTax.Cashbill.Example.csharp/Properties/AssemblyInfo.cs
+++ popbill.HomeTax.Cashbill.Example.csharp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리의 일반 정보는 다음 특성 집합을 통해 제어됩니다.
 // 어셈블리와 관련된 정보를 수정하려면
 // 이 특성 값을 변경하십시오.
-[assembly: AssemblyTitle("popbill.HomeTax.Cashbill.Example.csharp")]
+[assembly: AssemblyTitle("Popbill.HomeTax.Cashbill.Example.csharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("popbill.HomeTax.Cashbill.Example.csharp")]
+[assembly: AssemblyProduct("Popbill.HomeTax.Cashbill.Example.csharp")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
