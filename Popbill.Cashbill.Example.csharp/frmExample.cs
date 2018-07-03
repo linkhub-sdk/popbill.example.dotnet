87a0242c77003d50fd7d00aa99bbbb57b21109d5
kimhyunjin <khjzzm@gmail.com>
2018-07-03 15:29:35 +0900

    cashbill emailConfig emailtype(CSH_CANCEL) typo edit

--- Popbill.Cashbill.Example.csharp/frmExample.cs
+++ Popbill.Cashbill.Example.csharp/frmExample.cs
@@ -1589,7 +1589,7 @@ namespace Popbill.Cashbill.Example.csharp
                 foreach (EmailConfig info in resultList)
                 {
                     if (info.emailType == "CSH_ISSUE") tmp += "CSH_ISSUE (고객에게 현금영수증이 발행 되었음을 알려주는 메일) | " + info.sendYN + CRLF;
-                    if (info.emailType == "CSH_CANCELISSUE") tmp += "CSH_CANCELISSUE (고객에게 현금영수증이 발행취소 되었음을 알려주는 메일) | " + info.sendYN + CRLF;
+                    if (info.emailType == "CSH_CANCEL") tmp += "CSH_CANCEL (고객에게 현금영수증이 발행취소 되었음을 알려주는 메일) | " + info.sendYN + CRLF;
                 }
                 MessageBox.Show(tmp, "알림메일 전송목록 조회");
             }
@@ -1604,7 +1604,7 @@ namespace Popbill.Cashbill.Example.csharp
            현금영수증 메일전송 항목에 대한 전송여부를 수정한다.
            메일전송유형
            CSH_ISSUE : 고객에게 현금영수증이 발행 되었음을 알려주는 메일 입니다.
-           CSH_CANCELISSUE : 고객에게 현금영수증 발행취소 되었음을 알려주는 메일 입니다.
+           CSH_CANCEL : 고객에게 현금영수증 발행취소 되었음을 알려주는 메일 입니다.
         */
 
         private void btnUpdateEmailConfig_Click(object sender, EventArgs e)
