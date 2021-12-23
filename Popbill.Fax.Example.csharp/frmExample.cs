c6aabc07d5f1b6303e844e686d9a4732eb172217
ProWON <pro_won45@naver.com>
2021-12-23 16:57:22 +0900

    fixed email address, removed tel

--- Popbill.Fax.Example.csharp/frmExample.cs
+++ Popbill.Fax.Example.csharp/frmExample.cs
@@ -3,8 +3,8 @@
  *
  * - DotNet C# SDK 연동환경 설정방법 안내 : [개발가이드] - https://docs.popbill.com/fax/tutorial/dotnet#csharp
  * - 업데이트 일자 : 2021-12-23
- * - 연동 기술지원 연락처 : 1600-9854 / 070-4304-2991~2
- * - 연동 기술지원 이메일 : code@linkhub.co.kr
+ * - 연동 기술지원 연락처 : 1600-9854
+ * - 연동 기술지원 이메일 : code@linkhubcorp.com
  *
  * <테스트 연동개발 준비사항>
  * 1) 29, 32 라인에 선언된 링크아이디(LinkID)와 비밀키(SecretKey)를
--- Popbill.Hometax.Taxinvoice.Example.csharp/frmExample.cs
+++ Popbill.Hometax.Taxinvoice.Example.csharp/frmExample.cs
@@ -3,8 +3,8 @@
  *
  * - DotNet SDK 연동환경 설정방법 안내 : [개발가이드] - https://docs.popbill.com/httaxinvoice/tutorial/dotnet
  * - 업데이트 일자 : 2021-08-05
- * - 연동 기술지원 연락처 : 1600-9854 / 070-4304-2991
- * - 연동 기술지원 이메일 : code@linkhub.co.kr
+ * - 연동 기술지원 연락처 : 1600-9854
+ * - 연동 기술지원 이메일 : code@linkhubcorp.com
  *
  * <테스트 연동개발 준비사항>
  * 1) 32, 35 라인에 선언된 링크아이디(LinkID)와 비밀키(SecretKey)를
