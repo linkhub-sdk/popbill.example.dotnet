078a6b71798a671d1e644a2dd1b2efcdd6f47139
AlBan93 <albaneo0724@gmail.com>
2023-06-10 23:18:30 +0900

    현금영수증 - RegistIssue 응답 필드 중 tradeDT 제거

--- Popbill.Cashbill.Example.csharp/frmExample.cs
+++ Popbill.Cashbill.Example.csharp/frmExample.cs
@@ -176,8 +176,7 @@ namespace Popbill.Cashbill.Example.csharp
                 MessageBox.Show("응답코드(code) : " + response.code.ToString() + CRLF +
                                 "응답메시지(message) : " + response.message + CRLF +
                                 "국세청 승인번호(confirmNum) : " + response.confirmNum + CRLF +
-                                "거래일자(tradeDate) : " + response.tradeDate + CRLF +
-                                "거래일시(tradeDT) : " + response.tradeDT, "현금영수증 즉시발행");
+                                "거래일자(tradeDate) : " + response.tradeDate, "현금영수증 즉시발행");
             }
             catch (PopbillException ex)
             {
